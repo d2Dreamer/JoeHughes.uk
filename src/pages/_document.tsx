e099e063e9b04adf8823a91ffb586e4b28b9178f
Joseph Hughes <41088824+d2Dreamer@users.noreply.github.com>
2023-03-25 08:36:50 +0000

    Error Fixing

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,65 +1,39 @@
+import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { ThemeContext } from '@emotion/react';
 import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 import theme from '../styles/theme';
 
-export default class MyDocument extends Document {
-  static async getInitialProps(ctx: any) {
-    const originalRenderPage = ctx.renderPage;
-
-    try {
-      let styles = null;
-      const { theme } = require('../styles/theme');
-
-      const enhancedRenderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App: any) => (props: any) => (
-            <ThemeContext.Provider value={theme}>
-              <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <App {...props} />
-              </ThemeProvider>
-            </ThemeContext.Provider>
-          ),
-        });
-
-      const renderedPage = await enhancedRenderPage();
-      styles = (
-        <>
-          {renderedPage.styles}
-          <style
-            data-emotion-css={renderedPage.ids.join(' ')}
-            dangerouslySetInnerHTML={{ __html: renderedPage.css }}
-          />
-        </>
-      );
-
-      return {
-        ...renderedPage,
-        styles,
-      };
-    } finally {
-    }
-  }
-
+class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          {/* Set the page's viewport to properly scale on mobile devices */}
-          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-
-          {/* Set the page's theme colors */}
-          <meta name="theme-color" content={theme.palette.primary.main} />
-
-          {/* Add any additional head elements you need */}
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+          <style jsx global>{`
+            html,
+            body {
+              height: 100%;
+              margin: 0;
+            }
+            #__next {
+              height: 100%;
+            }
+          `}</style>
         </Head>
         <body>
-          <Main />
-          <NextScript />
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Main />
+            <NextScript />
+          </ThemeProvider>
         </body>
       </Html>
     );
   }
 }
+
+export default MyDocument;
