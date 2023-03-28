e087334304a88f1856e4639bd941d6e148c67717
Joseph Hughes <41088824+d2Dreamer@users.noreply.github.com>
2023-03-28 12:10:03 +0100

    Added CV download button to NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,12 @@
 import { Box, Button, Toolbar, AppBar, Typography } from "@mui/material";
+import GetAppIcon from "@mui/icons-material/GetApp";
 import Link from "next/link";
 
 const NavBar = () => {
+  const handleDownload = () => {
+    // Code to download PDF goes here
+  };
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -15,16 +20,28 @@ const NavBar = () => {
         </Box>
         <Box>
           <Button color="inherit" sx={{ mr: 2 }}>
-            <Link href="/contact">Contact</Link>
+            <Link href="/about">01. About</Link>
           </Button>
           <Button color="inherit" sx={{ mr: 2 }}>
-            <Link href="/work">Work</Link>
+            <Link href="/experience">02. Experience</Link>
           </Button>
           <Button color="inherit" sx={{ mr: 2 }}>
-            <Link href="/skills">Skills</Link>
+            <Link href="/skills">03. Skills</Link>
           </Button>
           <Button color="inherit" sx={{ mr: 2 }}>
-            <Link href="/about">About</Link>
+            <Link href="/work">04. Work</Link>
+          </Button>
+          <Button color="inherit" sx={{ mr: 2 }}>
+            <Link href="/contact">05. Contact</Link>
+          </Button>
+          <Button
+            color="inherit"
+            variant="outlined"
+            startIcon={<GetAppIcon />}
+            sx={{ ml: 2 }}
+            onClick={handleDownload}
+          >
+            Resume
           </Button>
         </Box>
       </Toolbar>
